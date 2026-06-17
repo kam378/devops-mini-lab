1db9c7357abc2e06395e300322570bedda061016
Ikedi-21 <ikedinachiogbonne@gmail.com>
2026-06-17 16:43:26 +0100

    test: add verification for subtraction

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,6 @@
-from calculator import calculate_area
+from calculator import calculate_area, subtract
 
 def test_area():
     assert calculate_area(5, 4) == 20
+def test_subtract():
+    assert subtract(10, 4) == 6 
